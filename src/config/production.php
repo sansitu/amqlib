21ed12412d3cbeefc25cb685d14a425618b3bdeb
sansitu <santosh_dms2003@yahoo.com>
2019-08-24 11:33:53 +0530

    Some changes in code file

--- src/config/production.php
+++ src/config/production.php
@@ -8,6 +8,6 @@ const AMQP_USERNAME = "guest";
 const AMQP_PASSWORD = "guest";
 const AMQP_VHOST = '/';
 
-const API_URL = 'https://api.mpokket.app/amq/v1/messages';
-const API_USERNAME = 'mpokket';
-const API_PASSWORD = 'api';
+const API_URL = 'https://prod.mpokket.org/amq/v1/messages';
+const API_USERNAME = 'mpokket123';
+const API_PASSWORD = 'api123';
--- src/config/staging.php
+++ src/config/staging.php
@@ -8,6 +8,6 @@ const AMQP_USERNAME = "root";
 const AMQP_PASSWORD = "root";
 const AMQP_VHOST = 'stg';
 
-const API_URL = 'https://stg.mpokket.app/amq/v1/messages';
-const API_USERNAME = 'mpokket';
-const API_PASSWORD = 'api';
+const API_URL = 'https://devstg.mpokket.org/amq/v1/messages';
+const API_USERNAME = 'mpokket123';
+const API_PASSWORD = 'api123';
